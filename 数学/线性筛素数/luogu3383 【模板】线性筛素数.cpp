ff2009718fed49af629d53ca6a0b997085ebc581
xxcc <784066071@qq.com>
2017-11-08 17:24:00 +0800

    添加线性筛素数

---
+++ 数学/线性筛素数/luogu3383 【模板】线性筛素数.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+const int N = 1e7+5;
+bool p[N];
+
+int main() {
+	int n, m;
+	scanf("%d%d", &n, &m);
+	p[0] = p[1] = true;
+	for (int i = 2; i <= n; i++)
+		if (!p[i])
+			for (int j = i << 1; j <= n; j += i)  // 从i*2开始，不然i会被置为合数 
+				p[j] = true;
+	for (int u; m--; ) {
+		scanf("%d", &u);
+		printf(p[u] ? "No\n" : "Yes\n");
+	}
+	return 0;
+}
+
