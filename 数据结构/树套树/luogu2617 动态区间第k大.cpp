f7f91ba131b2fc595c76f44ac93c6be0dda2d2a6
xxcc <784066071@qq.com>
2018-01-21 11:12:08 +0800

    修改树套树

--- 数据结构/树套树/luogu2617 动态区间第k大.cpp
+++ 数据结构/树套树/luogu2617 动态区间第k大.cpp
@@ -1,93 +1,81 @@
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
 #include <cstdlib>
-#include <cstring>
 #include <cstdio>
+#include <cstring>
+#include <cmath>
 #include <vector>
 using namespace std;
-const int N = 10050, M = 10050;
+const int N = 1e4+50, SIZ = 9e6+50;
+const int MI = 0, MX = 1e9;
 
-struct Opt {
-	bool isQ;
-	int l, r, k;
-	int pos, val;
-}opt[N*600];
-
-struct Node {
-	int ch[2], sum;
-}tr[N*600];
-int top = 1;
-
-int n, a[N];
-int rk[N<<1], dif;
-int root[N];
-vector<int> qNode[2];
-
-int lowbit(int x) { return x & (-x); }
-
-void update1(int &u, int L, int R, int pos, int val) {  // 权值线段树单点修改 
-	tr[u ? u : u = top++].sum += val;
-	if (L == R) return;
-	int mid = (L + R) >> 1;
-	if (pos <= mid) update1(tr[u].ch[0], L, mid, pos, val);
-	else update1(tr[u].ch[1], mid + 1, R, pos, val);
-}
-
-void update(int cur, int num, int val) {  // 树状数组单点修改，把与位置cur有关系的线段树全部修改 
-	int pos = lower_bound(rk + 1, rk + dif + 1, num) - rk;
-	for (int u = cur; u <= n; u += lowbit(u))
-		update1(root[u], 1, dif, pos, val);
-}
-
-int kth1(int L, int R, int k) {  // 权值线段树找第k大 
-	if (L == R) return L;
-	int mid = (L + R) >> 1, lCnt = 0;
-	for (int i = 0; i < 2; i++)
-		for (int j = 0, siz = qNode[i].size(); j < siz; j++)
-			lCnt += tr[tr[qNode[i][j]].ch[0]].sum * (i ? 1 : -1);
-	bool p = k > lCnt;
-	for (int i = 0; i < 2; i++)
-		for (int j = 0, siz = qNode[i].size(); j < siz; j++)
-			qNode[i][j] = tr[qNode[i][j]].ch[p];
-	return !p ? kth1(L, mid, k) : kth1(mid + 1, R, k - lCnt);
-}
-
-int kth(int l, int r, int k) {  // 树状数组查询组成1~l-1和1~r分别需要哪几棵线段树 
-	qNode[0].clear(); qNode[1].clear();
-	for (int i = l - 1; i; i -= lowbit(i))
-		qNode[0].push_back(root[i]);
-	for (int i = r; i; i -= lowbit(i))
-		qNode[1].push_back(root[i]);
-	return rk[kth1(1, dif, k)];
-}
-
-/*
-题目大意：动态区间第k大（单点修改）
-分析：
-主席树不支持修改，这类题目只能用树套树解决，这题用的是树状数组套线段树
-外层树状数组维护要组成位置区间1~i的权值线段树需要哪几棵线段树
-内层权值线段树维护在当前位置区间有哪些数
-这样就可以用O(nlog2n)的时间快速得到区间l~r的权值线段树
-空间O(nlognlogL)，L代表数字范围，可以离散化 
-*/
-int main() {
-	ios::sync_with_stdio(false);
-	int m; cin >> n >> m;
-	for (int i = 1; i <= n; i++)
-		cin >> a[i], rk[++dif] = a[i];
-	for (int i = 0; i < m; i++) {
-		char ch; cin >> ch;
-		if (opt[i].isQ = ch == 'Q') cin >> opt[i].l >> opt[i].r >> opt[i].k;
-		else cin >> opt[i].pos >> opt[i].val, rk[++dif] = opt[i].val;
+struct Tree {
+	struct Node {
+		int ch[2], cnt;
+	} seg[SIZ];
+	int top;
+	int n, root[N];
+	
+	Tree(): top(1) {}
+	void Init(int _n) { n = _n; }
+	int Lowbit(int x) { return x & -x; }
+	
+	void SegAdd(int &u, int L, int R, int pos, int val) {
+		seg[u ? u : u = top++].cnt += val;
+		if (L == R) return;
+		int mid = L + R >> 1;
+		if (pos <= mid) SegAdd(seg[u].ch[0], L, mid, pos, val);
+		else SegAdd(seg[u].ch[1], mid + 1, R, pos, val);
 	}
-	sort(rk + 1, rk + dif + 1);
-	dif = unique(rk + 1, rk + dif + 1) - (rk + 1);
-	for (int i = 1; i <= n; i++)
-		update(i, a[i], 1);
-	for (int i = 0; i < m; i++)
-		if (opt[i].isQ)	cout << kth(opt[i].l, opt[i].r, opt[i].k) << endl;
-		// 修改：先把原来的值的出现次数-1，再把新值出现次数+1 
-		else update(opt[i].pos, a[opt[i].pos], -1), update(opt[i].pos, a[opt[i].pos] = opt[i].val, 1);
-    return 0;
+	
+	void Add(int t, int pos, int val) {
+		for (int u = t; u <= n; u += Lowbit(u))
+			SegAdd(root[u], MI, MX, pos, val);
+	}
+	
+	int SegKth(vector<int> qu[2], int L, int R, int k) {
+		if (L == R) return L;
+		int mid = L + R >> 1, lCnt = 0;
+		for (int i = 0; i < 2; i++)
+			for (int siz = qu[i].size(), j = 0; j < siz; j++)
+				lCnt += seg[seg[qu[i][j]].ch[0]].cnt * (i ? 1 : -1);
+		bool lr = k > lCnt;
+		for (int i = 0; i < 2; i++)
+			for (int siz = qu[i].size(), j = 0; j < siz; j++)
+				qu[i][j] = seg[qu[i][j]].ch[lr];
+		return !lr ? SegKth(qu, L, mid, k) : SegKth(qu, mid + 1, R, k - lCnt);
+	}
+	
+	int Kth(int t1, int t2, int k) {
+		vector<int> qu[2];
+		for (int u = t1 - 1; u; u -= Lowbit(u))
+			qu[0].push_back(root[u]);
+		for (int u = t2; u; u -= Lowbit(u))
+			qu[1].push_back(root[u]);
+		return SegKth(qu, MI, MX, k);
+	}
+} tree;
+
+int a[N];
+
+int main() {
+	int n, m; scanf("%d%d", &n, &m); tree.Init(n);
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
+		tree.Add(i, a[i], 1);
+	}
+	while (m--) {
+		char opt[3]; int r1, r2, r3;
+		scanf("%s%d%d", opt, &r1, &r2);
+		if (opt[0] == 'Q') {
+			scanf("%d", &r3);
+			printf("%d\n", tree.Kth(r1, r2, r3));
+		} else {
+			tree.Add(r1, a[r1], -1);
+			a[r1] = r2;
+			tree.Add(r1, a[r1], 1);
+		}
+	}
+	return 0;
 }
 
