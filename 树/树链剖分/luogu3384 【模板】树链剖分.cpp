1ed3f8807a1a9c945b413039d966f0971aa3bb4f
xxccxcxc <784066071@qq.com>
2018-02-04 21:26:43 +0800

    修改树链剖分

--- 树/树链剖分/luogu3384 【模板】树链剖分.cpp
+++ 树/树链剖分/luogu3384 【模板】树链剖分.cpp
@@ -3,157 +3,169 @@
 #include <cstdlib>
 #include <cstring>
 #include <cstdio>
+#include <cmath>
 using namespace std;
 typedef long long LL;
-const int N = 200050;
+const int N = 1e5+50;
 
-struct Seg {
-	int l, r;
-	LL sum, lazy;
-	int size() { return r - l + 1; }
-	void mod(LL mo) { sum %= mo; lazy %= mo; }
-}seg[N<<2];
+struct Mint {
+	static LL mod;
+	LL x;
+	
+	Mint(LL _x=0) { x = (_x % mod + mod) % mod; }
+	friend Mint operator + (const Mint &a, const Mint &b) { return Mint(a.x + b.x); }
+	Mint operator += (const Mint &b) { return *this = *this + b; }
+	Mint operator - () const { return Mint(-x); }
+	friend Mint operator - (const Mint &a, const Mint &b) { return Mint(a.x - b.x); }
+	Mint operator -= (const Mint &b) { return *this = *this - b; }
+	friend Mint operator * (const Mint &a, const Mint &b) { return Mint(a.x * b.x % mod); }
+	Mint operator *= (const Mint &b) { return *this = *this * b; }
+	friend istream& operator >> (istream &in, Mint &a) { in >> a.x; return in; }
+	friend ostream& operator << (ostream &out, const Mint &a) { out << a.x; return out; }
+};
+LL Mint::mod = 5;
 
-LL mo;
+struct Graph {
+	struct Edge {
+		int to, nxt;
+		Edge() {}
+		Edge(int _to, int _nxt): to(_to), nxt(_nxt) {}
+	} e[N<<1];
+	int top, head[N];
+	
+	Graph(): top(0) { memset(head, -1, sizeof(head)); }
+	void Add1(int u, int v) { e[top] = Edge(v, head[u]); head[u] = top++; }
+	void Add(int u, int v) { Add1(u, v); Add1(v, u); }
+};
 
-int son(int u, int p) { return u << 1 | p; }
-
-void pushup(int u) { seg[u].sum = (seg[son(u, 0)].sum + seg[son(u, 1)].sum) % mo; }
-
-void build(int u, int l, int r, LL *a) {
-	seg[u].l = l; seg[u].r = r;
-	if (l == r) { seg[u].sum = a[l] % mo; return; }
-	int mid = (l + r) >> 1;
-	build(son(u, 0), l, mid, a);
-	build(son(u, 1), mid+1, r, a);
-	pushup(u);
-}
-
-void addVal(int u, LL val) {
-	seg[u].sum += seg[u].size() * val;
-	seg[u].lazy += val;
-	seg[u].mod(mo);
-}
-
-void pushdown(int u) {
-	for (int i = 0; i <= 1; i++)
-		addVal(son(u, i), seg[u].lazy);
-	seg[u].lazy = 0;
-}
-
-void update(int u, int l, int r, LL val) {
-	if (seg[u].l == l && seg[u].r == r) { addVal(u, val); return; }
-	if (seg[u].lazy) pushdown(u);
-	int mid = (seg[u].l + seg[u].r) >> 1;
-	if (l <= mid) update(son(u, 0), l, min(r, mid), val);
-	if (r > mid) update(son(u, 1), max(l, mid+1), r, val);
-	pushup(u);  // pushup别忘记写 
-}
-
-LL query(int u, int l, int r) {
-	if (seg[u].l == l && seg[u].r == r) return seg[u].sum;
-	if (seg[u].lazy) pushdown(u);
-	LL ret = 0; int mid = (seg[u].l + seg[u].r) >> 1;
-	if (l <= mid) ret += query(son(u, 0), l, min(r, mid));
-	if (r > mid) ret += query(son(u, 1), max(l, mid+1), r);
-	return ret %= mo;  // 记得mod 
-}
-
-struct Edge {
-	int to, nxt;
-	Edge(int _to=0, int _nxt=0): to(_to), nxt(_nxt) {}
-}e[N<<1];
-
-int ecnt, head[N];
-
-void add(int u, int v) {
-	e[ecnt] = Edge(v, head[u]); head[u] = ecnt++;
-	e[ecnt] = Edge(u, head[v]); head[v] = ecnt++;
-}
-
-struct SpNode {
-	int fa, son, siz, dep, id, top;
-	SpNode(int _fa=0, int _son=0, int _siz=0, int _dep=0, int _id=0, int _top=0) {
-		fa = _fa; son = _son; siz = _siz; dep = _dep; id = _id; top = _top;
+struct BIT {
+	int n;
+	Mint b[N], c[N];
+	
+	int Lowbit(int x) { return x & -x; }
+	void InitSize(int _n) { n = _n; }
+	
+	void Add(Mint *t, int pos, Mint val) {
+		for (int i = pos; i <= n; i += Lowbit(i))
+			t[i] += val;
 	}
-}sp[N];
-
-void dfs1(int u, int fa) {
-	sp[u] = SpNode(fa, 0, 1, sp[fa].dep+1);
-	for (int i = head[u]; ~i; i = e[i].nxt) {
-		int v = e[i].to;
-		if (v == fa) continue;
-		dfs1(v, u);
-		if (sp[v].siz > sp[sp[u].son].siz)
-			sp[u].son = v;
-		sp[u].siz += sp[v].siz;
+	
+	void Add(int l, int r, Mint val) {
+		Add(b, l, val); Add(b, r+1, -val);
+		Add(c, l, val*(l-1)); Add(c, r+1, -val*r);
 	}
-}
-
-int dfscnt = 0;
-LL a[N], segInit[N];
-
-void dfs2(int u, int curTop) {
-	sp[u].id = ++dfscnt;
-	segInit[sp[u].id] = a[u];
-	sp[u].top = curTop;
-	if (!sp[u].son) return;
-	dfs2(sp[u].son, curTop);
-	for (int i = head[u]; ~i; i = e[i].nxt) {
-		int v = e[i].to;
-		if (v != sp[u].fa && v != sp[u].son)
-			dfs2(v, v);
+	
+	Mint Query(Mint *t, int pos) {
+		Mint ret = 0;
+		for (int i = pos; i > 0; i -= Lowbit(i))
+			ret += t[i];
+		return ret;
 	}
-}
-
-void pathModify(int u, int v, LL val) {  // 路径修改 
-	int tu = sp[u].top, tv = sp[v].top;
-	while (tu != tv) {
-		if (sp[tu].dep > sp[tv].dep)
-			swap(u, v), swap(tu, tv);
-		update(1, sp[tv].id, sp[v].id, val);
-		v = sp[tv].fa; tv = sp[v].top;
+	
+	Mint Query(int l, int r) {
+		Mint s1 = (l-1) * Query(b, l-1) - Query(c, l-1);
+		Mint s2 = r * Query(b, r) - Query(c, r);
+		return s2 - s1;
 	}
-	if (sp[u].dep > sp[v].dep) swap(u, v);
-	update(1, sp[u].id, sp[v].id, val);
-}
+};
 
-LL pathQuery(int u, int v) {  // 路径查询 
-	int tu = sp[u].top, tv = sp[v].top;
-	LL ret = 0;
-	while (tu != tv) {
-		if (sp[tu].dep > sp[tv].dep)
-			swap(u, v), swap(tu, tv);
-		ret = (ret + query(1, sp[tv].id, sp[v].id)) % mo;
-		v = sp[tv].fa; tv = sp[v].top;
+struct ChainDevide {
+	Graph G;
+	BIT bit;
+	int dfsCnt;
+	
+	struct Node {
+		int fa, dep, siz, son, top, id;
+	} tr[N];
+	
+	void AddEdge(int u, int v) { G.Add(u, v); }
+	
+	void Devide(int n, int root, const int *val) {
+		bit.InitSize(n);
+		Dfs1(root, 0); Dfs2(root, root, val);
 	}
-	if (sp[u].dep > sp[v].dep) swap(u, v);
-	return ret = (ret + query(1, sp[u].id, sp[v].id)) % mo;
-}
+	
+	void Dfs1(int u, int fa) {
+		tr[u].fa = fa;
+		tr[u].dep = tr[fa].dep + 1;
+		tr[u].siz = 1; tr[u].son = 0;
+		for (int i = G.head[u]; ~i; i = G.e[i].nxt) {
+			int v = G.e[i].to;
+			if (v == fa) continue;
+			Dfs1(v, u);
+			tr[u].siz += tr[v].siz;
+			if (tr[v].siz > tr[tr[u].son].siz)
+				tr[u].son = v;
+		}
+	}
+	
+	void Dfs2(int u, int top, const int *val) {
+		tr[u].top = top;
+		tr[u].id = ++dfsCnt;
+		bit.Add(tr[u].id, tr[u].id, val[u]);
+		if (!tr[u].son) return;
+		Dfs2(tr[u].son, top, val);
+		for (int i = G.head[u]; ~i; i = G.e[i].nxt) {
+			int v = G.e[i].to;
+			if (v != tr[u].fa && v != tr[u].son)
+				Dfs2(v, v, val);
+		}
+	}
+	
+	void PathAdd(int u, int v, Mint val) {
+		int tu = tr[u].top, tv = tr[v].top;
+		while (tu != tv) {
+			if (tr[tu].dep > tr[tv].dep)
+				swap(u, v), swap(tu, tv);
+			bit.Add(tr[tv].id, tr[v].id, val);
+			v = tr[tv].fa; tv = tr[v].top;
+		}
+		if (tr[u].dep > tr[v].dep) swap(u, v);
+		bit.Add(tr[u].id, tr[v].id, val);
+	}
+	
+	Mint PathQuery(int u, int v) {
+		int tu = tr[u].top, tv = tr[v].top;
+		Mint ret = 0;
+		while (tu != tv) {
+			if (tr[tu].dep > tr[tv].dep)
+				swap(u, v), swap(tu, tv);
+			ret += bit.Query(tr[tv].id, tr[v].id);
+			v = tr[tv].fa; tv = tr[v].top;
+		}
+		if (tr[u].dep > tr[v].dep) swap(u, v);
+		return ret += bit.Query(tr[u].id, tr[v].id);
+	}
+	
+	void TreeAdd(int u, Mint val) {
+		bit.Add(tr[u].id, tr[u].id + tr[u].siz - 1, val);
+	}
+	
+	Mint TreeQuery(int u) {
+		return bit.Query(tr[u].id, tr[u].id + tr[u].siz - 1);
+	}
+} tree;
 
-// 子树修改（利用子树的dfn是连续的） 
-void treeModify(int u, LL val) { update(1, sp[u].id, sp[u].id + sp[u].siz - 1, val); }
-
-// 子树查询 
-LL treeQuery(int u) { return query(1, sp[u].id, sp[u].id + sp[u].siz - 1); }
+int r[N];
 
 int main() {
-	memset(head, -1, sizeof(head));
-	int n, m, root;
-	scanf("%d%d%d%lld", &n, &m, &root, &mo);
+	ios::sync_with_stdio(false);
+	int n, m, root; cin >> n >> m >> root >> Mint::mod;
 	for (int i = 1; i <= n; i++)
-		scanf("%lld", &a[i]);
-	for (int i = 1, u, v; i < n; add(u, v), i++)
-		scanf("%d%d", &u, &v);
-	dfs1(root, 0); dfs2(root, root); build(1, 1, n, segInit);
+		cin >> r[i];
+	for (int u, v, i = 1; i < n; i++) {
+		cin >> u >> v;
+		tree.AddEdge(u, v);
+	}
+	tree.Devide(n, root, r);
 	while (m--) {
-		int opt, u, v; LL val;
-		scanf("%d", &opt);
+		int opt, u, v; Mint val;
+		cin >> opt;
 		switch (opt) {
-			case 1: scanf("%d%d%lld", &u, &v, &val); pathModify(u, v, val);             break; 
-			case 2: scanf("%d%d", &u, &v);           printf("%lld\n", pathQuery(u, v)); break;
-			case 3: scanf("%d%lld", &u, &val);       treeModify(u, val);                break;
-			case 4: scanf("%d", &u);                 printf("%lld\n", treeQuery(u));    break;
+			case 1: cin >> u >> v >> val; tree.PathAdd(u, v, val);               break;
+			case 2: cin >> u >> v;        cout << tree.PathQuery(u, v) << endl;  break;
+			case 3: cin >> u >> val;      tree.TreeAdd(u, val);                  break;
+			case 4: cin >> u;             cout << tree.TreeQuery(u) << endl;     break;
 		}
 	}
     return 0;
